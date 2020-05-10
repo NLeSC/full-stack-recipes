b233b78e4b60483aae36d4760d0138a0323a254a
Jesús García <ctw@ctwhome.com>
2020-05-10 10:11:13 +0200

    workbox debug options to false

--- recipes/nuxt-apollo-hasura/nuxt.config.js
+++ recipes/nuxt-apollo-hasura/nuxt.config.js
@@ -96,15 +96,15 @@ export default {
     },
   },
 
-  /*pwa: {
+  pwa: {
     workbox: {
       // importScripts: ['/firebase-auth-sw.js'],
       // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
       // only set this true for testing and remember to always clear your browser cache in development
-      dev: process.env.NODE_ENV === 'development',
+      // dev: process.env.NODE_ENV === 'development',
       debug: false,
     },
-  },*/
+  },
 
   /**
    * Apollo
