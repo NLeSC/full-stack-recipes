727ebb77fc2f0614f1a6af1b2837ce4561cc7ab2
Jesús García <ctw@ctwhome.com>
2020-05-10 15:11:03 +0200

    Home page image

--- recipes/nuxt-apollo-hasura/pages/index.vue
+++ recipes/nuxt-apollo-hasura/pages/index.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="d-flex flex-column justify-center align-center">
-    <v-card max-width="700" width="100%">
-      <v-img src="images/stack.png" :aspect-ratio="4 / 1" />
+    <v-img max-width="700" width="100%" src="images/stack.jpg" class="mt-4" />
 
+    <v-card max-width="700" width="100%" class="mt-4" outlined>
       <v-card-title class="headline">
         NuxtJs + Apollo + Hasura + Postgres template
       </v-card-title>
 
       <v-card-text>
+        <v-icon>mdi-database</v-icon> Database:
+        <a href="http://localhost:4000" target="_blank" class="link"
+          >Hasura console</a
+        >
         <hr class="my-3" />
-        Database:
-        <a href="http://localhost:4000" target="_blank">Hasura console</a>
-        <br />
-        <a href="https://nuxtjs.org/" target="_blank">
-          Nuxt Documentation
-        </a>
-        <br />
-        <a href="https://materialdesignicons.com/" target="_blank">
-          Material Design Icons
-        </a>
-        <br />
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-          Nuxt GitHub
-        </a>
-        Nuxt modules: https://auth.nuxtjs.org/ apollo
+        <v-row>
+          <v-col sm="6" cols="12" class="mb-4"
+            ><h4>Perks</h4>
+            <ul>
+              <li>Vue Nuxt Structure</li>
+              <li>Auth Login with user roles</li>
+              <li>Private Auth pages</li>
+              <li>Realtime subscription</li>
+              <li>Optimistic UI</li>
+              <li>One command development setup with docker</li>
+              <li>Responsive UI</li>
+            </ul>
+          </v-col>
+          <v-col sm="6" cols="12"
+            ><h4>Documentation</h4>
+            <div v-for="link in links" :key="link.name">
+              <v-icon>mdi-link</v-icon>
+              <a :href="link.link" target="_blank">{{ link.name }}</a>
+            </div></v-col
+          >
+        </v-row>
       </v-card-text>
 
       <v-card-actions>
@@ -35,8 +45,47 @@
     </v-card>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      links: [
+        {
+          name: 'Nuxt Documentation',
+          link: 'https://nuxtjs.org/',
+        },
+        {
+          name: 'Nuxt Apollo',
+          link: 'https://auth.nuxtjs.org/',
+        },
+        {
+          name: 'Nuxt Firebase',
+          link: '',
+        },
+        {
+          name: 'Hasura documentation',
+          link: 'https://hasura.io/vue-graphql/',
+        },
+        {
+          name: 'Vuetify',
+          link: 'https://vuetifyjs.com/en/components',
+        },
+        {
+          name: 'Material Design Icons',
+          link: 'https://materialdesignicons.com/',
+        },
+      ],
+    }
+  },
 }
 </script>
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
